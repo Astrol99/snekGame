eeea8cdbb72a50109259a613acf8b2015c0761af
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2021-11-20 01:08:57 -0600

    idk i wanna go sleep

--- Cell.py
+++ Cell.py
@@ -1,20 +1,19 @@
 import pygame
 
-class SnekPart:
-    def __init__(self, current, prev):
-        self.current = current
-        self.prev = prev
-
 class Cell:
     def __init__(self, surface, x, y, size, thickness=1, state=None):
         self.surface = surface
-        self.state = state
+        self.x = x
+        self.y = y
+        self.size = size
         self.thickness = thickness
-        self.borderRect = pygame.Rect(x, y, size, size)
-        self.innerRect = pygame.Rect(self.borderRect.x+self.thickness, 
-                                    self.borderRect.y+self.thickness, 
-                                    self.borderRect.w-self.thickness,
-                                    self.borderRect.h-self.thickness)
+        self.state = state
+
+        self.indexX = self.x//size
+        self.indexY = self.y//size
+
+        self.direction = None
+        self.prev = None
 
     def render(self):
         if self.state == "head":
@@ -25,6 +24,12 @@ class Cell:
             color = pygame.Color(255,0,0)
         else:
             color = pygame.Color(0,0,0)
+        
+        self.borderRect = pygame.Rect(self.x, self.y, self.size, self.size)
+        self.innerRect = pygame.Rect(self.borderRect.x+self.thickness, 
+                                    self.borderRect.y+self.thickness, 
+                                    self.borderRect.w-self.thickness,
+                                    self.borderRect.h-self.thickness)
 
         pygame.draw.rect(self.surface, (255,255,255), self.borderRect, width=self.thickness)  # Border
         pygame.draw.rect(self.surface, color, self.innerRect)
--- SnekGame.py
+++ SnekGame.py
@@ -6,7 +6,7 @@ class SnekGame:
     def __init__(self, width, height, size, thickness=1):
         self.displayRes = self.width, self.height = width, height
         self.size = size
-        self.thickness = thickness        
+        self.thickness = thickness
 
         # Init Window
         pygame.init()
@@ -25,20 +25,48 @@ class SnekGame:
                 row.append(cell)
             self.grid.append(row)
         
-        self.grid[0][0].state = "head"
-        self.grid[0][0].render()
+        # Init Snake
+        self.snekHead = self.grid[0][0]
+        self.snekHead.state = "head"
+        self.snekHead.render()
+
+        self.snekHead.direction = "right"
 
         self.main()
 
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self.running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                self.snekHead.direction = "up"
+            if event.key == pygame.K_DOWN:
+                self.snekHead.direction = "down"
+            if event.key == pygame.K_LEFT:
+                self.snekHead.direction = "left"
+            if event.key == pygame.K_RIGHT:
+                self.snekHead.direction = "right"
     
     def on_loop(self):
-        pass
+        temp = self.snekHead
+        temp.state = None
+
+        if 0 <= self.snekHead.x <= self.width and 0 <= self.snekHead.y <= self.height: 
+            if self.snekHead.direction == "up":
+                self.snekHead = self.grid[self.snekHead.indexX][self.snekHead.indexY+1]
+            elif self.snekHead.direction == "down":
+                self.snekHead = self.grid[self.snekHead.indexX][self.snekHead.indexY-1]
+            elif self.snekHead.direction == "left":
+                self.snekHead = self.grid[self.snekHead.indexX-1][self.snekHead.indexY]
+            elif self.snekHead.direction == "right":
+                self.snekHead = self.grid[self.snekHead.indexX+1][self.snekHead.indexY]
+
+        self.snekHead.prev = temp
+
 
     def on_render(self):
-        pass
+        self.snekHead.prev.render()
+        self.snekHead.render()
 
     def on_cleanup(self):
         pygame.quit()
